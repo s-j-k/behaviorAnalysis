f66d9df91105376c693b55c41e5321dd9f81d79e
su <68665480+s-j-k@users.noreply.github.com>
2024-10-15 23:57:24 -0400

    shits not working for the opto diff anova

--- optoDataStats.m
+++ optoDataStats.m
@@ -743,14 +743,14 @@ for jj=2:4
     allDataTestsOnly{jj,40}=mgbTempTestsOnly{jj,30}-mgbTempTestsOnly{jj,29};
     allDataTestsOnly{jj,41}=mgbTempTestsOnly{jj,32}-mgbTempTestsOnly{jj,31};
 end
-% allDataTestsOnly{4,33}=allDataTestsOnly{4,33}';
-
 diffFull=NaN; diffTone=NaN; diffChoice=NaN;
 for jj=2:size(mgbTempTestsOnly,1)
-    diffFull=cat(1,diffFull,mgbTempTestsOnly{jj,33});
-    diffTone=cat(1,diffTone,mgbTempTestsOnly{jj,34});
-    diffChoice=cat(1,diffChoice,mgbTempTestsOnly{jj,35});
+    diffFull=cat(1,diffFull,allDataTestsOnly{jj,39});
+    diffTone=cat(1,diffTone,allDataTestsOnly{jj,40});
+    diffChoice=cat(1,diffChoice,allDataTestsOnly{jj,41});
 end
+catMGBDiff=[diffFull(~isnan(diffFull)) diffTone(~isnan(diffTone)) diffChoice(~isnan(diffChoice))];
+aovdiffMGB=anova1(catMGBDiff)
 subplot(1,2,1)
 qqq=bar([nanmean(diffFull) nanmean(diffTone) nanmean(diffChoice)]); hold on;
 qqq(1).FaceColor='flat'; qqq(1).CData=[optocolor;optocolor;optocolor;];hold on;
@@ -766,22 +766,21 @@ title(['MGB Inactivation']);
 xticklabels({'Full Trial','Tone','Choice'});
 xlabel('Condition');
 
-% IC
 clear jj
-for jj=2:4
-    allDataTestsOnly{jj,39}=tempTestsOnly{jj,28}-tempTestsOnly{jj,27};
-    allDataTestsOnly{jj,40}=tempTestsOnly{jj,30}-tempTestsOnly{jj,29};
-    allDataTestsOnly{jj,41}=tempTestsOnly{jj,32}-tempTestsOnly{jj,31};
+for jj=2:4 %IC
+    allDataTestsOnly{jj,42}=tempTestsOnly{jj,33}-tempTestsOnly{jj,34};
+    allDataTestsOnly{jj,43}=tempTestsOnly{jj,35}-tempTestsOnly{jj,36};
+    allDataTestsOnly{jj,44}=tempTestsOnly{jj,37}-tempTestsOnly{jj,38};
 end
-% allDataTestsOnly{4,33}=allDataTestsOnly{4,33}';
-
 diffFull=NaN; diffTone=NaN; diffChoice=NaN;
 for jj=2:size(tempTestsOnly,1)
-    diffFull=cat(1,diffFull,tempTestsOnly{jj,33});
-    diffTone=cat(1,diffTone,tempTestsOnly{jj,34});
-    diffChoice=cat(1,diffChoice,tempTestsOnly{jj,35});
+    diffFull=cat(1,diffFull,allDataTestsOnly{jj,42});
+    diffTone=cat(1,diffTone,allDataTestsOnly{jj,43});
+    diffChoice=cat(1,diffChoice,allDataTestsOnly{jj,44});
 end
-subplot(1,2,1)
+catICDiff=[diffFull(~isnan(diffFull)) diffTone(~isnan(diffTone)) diffChoice(~isnan(diffChoice))];
+aovdiffMGB=anova1(catICDiff)
+subplot(1,2,2)
 qqq=bar([nanmean(diffFull) nanmean(diffTone) nanmean(diffChoice)]); hold on;
 qqq(1).FaceColor='flat'; qqq(1).CData=[optocolor;optocolor;optocolor;];hold on;
 scatter(repmat(qqq(1).XEndPoints(1),size(diffFull,1),1), ...
@@ -790,16 +789,14 @@ scatter(repmat(qqq(1).XEndPoints(2),size(diffTone,1),1), ...
     diffTone,'MarkerEdgeColor',[0 0 0],'MarkerFaceColor',optocolor);
 scatter(repmat(qqq(1).XEndPoints(3),size(diffChoice,1),1), ...
     diffChoice,'MarkerEdgeColor',[0 0 0],'MarkerFaceColor',optocolor);
-% [h,p,ci,stats] = ttest2(diffFull,diffTone); % replace this with the anova
-% sigstar({[1,2]}, p)
 title(['IC Inactivation']);
 xticklabels({'Full Trial','Tone','Choice'});
 xlabel('Condition');
 
 wwFig.Position(3:4)=[325 275];
 ylabel('Light on - Light off');
-saveas(gcf,['BySess_Difference_T_MGB_PercentCorrect_Opto']);
-saveas(gcf,['BySess_Difference_T_MGB_PercentCorrect_Opto.png']);
+saveas(gcf,['BySess_Difference_T_MGB_IC_PercentCorrect_Opto']);
+saveas(gcf,['BySess_Difference_T_MGB_IC_PercentCorrect_Opto.png']);
 
 %%
 % by animal
